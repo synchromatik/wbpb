232b2b9fa8946d95f23d234d39459385a01f9a23
Marko Manojlovic <marko@ivyexec.com>
2020-02-06 17:34:24 +0100

    mapbox

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,28 +2,68 @@ import React from 'react';
 import Map from 'pigeon-maps';
 import Overlay from 'pigeon-overlay';
 import PBMarker from '../../images/map-icon.png';
-// // Initial location of pb studio
-// const InitialState = {
-//   viewport: {
-//     latitude: 44.819406,
-//     longitude: 20.452809,
-//     zoom: 17.8,
-//     width: '100%',
-//     height: 400,
-//     mapStyle: 'mapbox://styles/mapbox/streets-v11',
-//   },
-// };
+
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoic3luY2hyb21hdGlrIiwiYSI6ImNrMjViajZxNTEyMG4zbm1xNG9oNzBldmUifQ.7LKfs33uDUzLePh054m3LA';
+
+const mapbox = (mapboxId, accessToken) => (x, y, z, dpr) => `https://api.mapbox.com/styles/v1/mapbox/${mapboxId}/tiles/256/${z}/${x}/${y}${dpr >= 2 ? '@2x' : ''}?access_token=${accessToken}`;
+
+const providers = {
+  streets: mapbox('streets-v10', MAPBOX_ACCESS_TOKEN),
+  satellite: mapbox('satellite-streets-v10', MAPBOX_ACCESS_TOKEN),
+  outdoors: mapbox('outdoors-v10', MAPBOX_ACCESS_TOKEN),
+  light: mapbox('light-v9', MAPBOX_ACCESS_TOKEN),
+  dark: mapbox('dark-v9', MAPBOX_ACCESS_TOKEN),
+};
+
+const initialMapState = {
+  center: [44.819406, 20.452809],
+  zoom: 17.8,
+  provider: 'streets',
+  metaWheelZoom: false,
+  twoFingerDrag: false,
+  animate: true,
+  animating: false,
+  zoomSnap: true,
+  mouseEvents: true,
+  touchEvents: true,
+  minZoom: 1,
+  maxZoom: 18,
+  dragAnchor: [48.8565, 2.3475],
+};
 
 function Mapa() {
+  const {
+    center,
+    zoom, provider,
+    metaWheelZoom,
+    twoFingerDrag,
+    zoomSnap,
+    mouseEvents,
+    touchEvents,
+    minZoom,
+    maxZoom,
+  } = initialMapState;
   return (
     <Map
-      center={[44.819406, 20.452809]}
-      zoom={17.8}
+      limitBounds="edge"
+      center={center}
+      zoom={zoom}
+      provider={providers[provider]}
+      dprs={[1, 2]}
+      metaWheelZoom={metaWheelZoom}
+      twoFingerDrag={twoFingerDrag}
+      zoomSnap={zoomSnap}
+      mouseEvents={mouseEvents}
+      touchEvents={touchEvents}
+      minZoom={minZoom}
+      maxZoom={maxZoom}
+      attribution={null}
+      defaultWidth={600}
       height={400}
+      boxClassname="pigeon-filters"
     >
       <Overlay
         anchor={[44.819406, 20.452809]}
-        offset={[43, 39]}
       >
         <img src={PBMarker} width={60} height={96} alt="PB Tattoo" />
       </Overlay>
